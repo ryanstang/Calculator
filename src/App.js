9d15c805709c70a3481e2ed7794630115968365f
ryanstang <ryanstang30@gmail.com>
2021-12-15 14:12:16 -0800

    equal sign working, implement mathjs

--- src/App.js
+++ src/App.js
@@ -4,39 +4,54 @@ import "@fontsource/roboto";
 import Header from "./components/Header";
 import Output from "./components/Output";
 import Input from "./components/Input";
+import { create, all } from "mathjs";
 
 function App() {
+  const config = {};
+  const math = create(all, config);
+
   const [calc, setCalc] = useState([
-    [7, 4, 1, 0],
-    [8, 5, 2, "."],
-    [9, 6, 3, "+/-"],
-    ["DEL", "*", "+", "ANS"],
-    ["AC", "/", "-", "="],
+    ["Null", 7, 4, 1, 0],
+    ["Null", 8, 5, 2, "."],
+    ["Null", 9, 6, 3, "+/-"],
+    ["(", "DEL", "*", "+", "ANS"],
+    [")", "AC", "/", "-", "="],
   ]);
 
-  const [currentCalculation, setCurrentCalculation] = useState(0);
+  const [currentCalculation, setCurrentCalculation] = useState([]);
   const [prevCalculation, setPrevCalculation] = useState(null);
+  const [prevButton, setPrevButton] = useState(null);
 
   // add to currentcalculation
   const makeCalculation = (func) => {
-    if (Number.isInteger(func)) {
-      setCurrentCalculation(currentCalculation * 10 + parseInt(func));
-    } else if (["+", "-", "/", "*"].includes(func)) {
-      console.log(["+", "-", "/", "*"]);
+    if (
+      Number.isInteger(func) ||
+      ["+", "-", "/", "*", "(", ")"].includes(func)
+    ) {
+      setCurrentCalculation([...currentCalculation, func]);
     } else if (func === "DEL") {
-      setCurrentCalculation(Math.floor(currentCalculation / 10));
+      setCurrentCalculation(currentCalculation.slice(0, -1));
     } else if (func === "AC") {
-      setCurrentCalculation(0);
+      setCurrentCalculation([]);
       setPrevCalculation(null);
     } else if (func === "+/-") {
-      setCurrentCalculation(currentCalculation *-1)
+      currentCalculation.length > 0 && currentCalculation[0] !== "-"
+        ? setCurrentCalculation(["-", ...currentCalculation])
+        : setCurrentCalculation(currentCalculation.slice(1));
+    } else if (func === "=") {
+      console.log(currentCalculation)
+      console.log(math.evaluate(currentCalculation.join("")))
+      setCurrentCalculation([math.evaluate(currentCalculation.join(""))])
     }
+    setPrevButton(func)
   };
 
   return (
     <div className="container">
       <Header />
-      <Output calculation={currentCalculation} />
+      <Output
+        calculation={currentCalculation.length > 0 ? currentCalculation : 0}
+      />
       <Input calcInfo={calc} makeCalculation={makeCalculation} />
     </div>
   );
